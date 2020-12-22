94beba4ed9d9ec4a18e1be48c175367e4097fb7b
aheisdorffer21 <73353675+aheisdorffer21@users.noreply.github.com>
2020-12-22 14:30:44 -0600

    Add files via upload

--- main.rs
+++ main.rs
@@ -1,11 +1,35 @@
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+
 fn main() {
-    let mut number = 3;
+    println!("Guess the number!");
 
-    while number != 0 {
-        println!("{}!", number);
+    let secret_number = rand::thread_rng().gen_range(1, 101);
 
-        number -= 1;
+    loop {
+        println!("Please input your guess.");
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Bigger!"),
+            Ordering::Greater => println!("Smaller!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
     }
- 
-    println!("LIFTOFF!!!");
 }
